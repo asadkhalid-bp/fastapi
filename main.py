da70237c05f53904187015335e1a195dd0d9cd68
asadkhalid-bp <asadkhalid@bitspro.com>
2022-12-05 12:02:55 +0500

    api

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    voting: Optional['int'] = None
+
+
 @app.get("/")
-async def root():
+def root():
     return {"message": "Hello World"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
+
+
+@app.post("/createposts")
+def create_posts(new_post: Post):
+    return {"data": new_post}
