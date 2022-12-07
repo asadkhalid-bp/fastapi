1e18280ed591752394fa2a9f29667261f54dd710
asadkhalid-bp <asadkhalid@bitspro.com>
2022-12-07 19:29:11 +0500

    fastapi

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from typing import Optional
+from datetime import datetime
 
 
 
@@ -11,3 +12,11 @@ class PostBase(BaseModel):
 
 class PostCreate(PostBase):
     pass
+
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+
+    class config:
+        orm_mode = True
